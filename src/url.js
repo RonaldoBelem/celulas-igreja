75969aa116c930478324a8a93a79a0150df6aa25
Igreja Ranking App <igreja-ranking@example.com>
2025-02-06 22:53:41 -0300

    att

--- src/url.js
+++ src/url.js
@@ -1,6 +1,8 @@
+const getBaseUrl = () => {
+  if (import.meta.env.PROD) {
+    return "https://celulas-igreja-api.onrender.com";
+  }
+  return "http://localhost:3001";
+};
 
-
-
-
-
-export const url = "http://localhost:3001";
+export const url = getBaseUrl();
