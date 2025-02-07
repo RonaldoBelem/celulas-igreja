52b98c1690f3b44a33392e3d319eee3e1dc312ab
RONALDO NUNES BELEM <98597641+RonaldoBelem@users.noreply.github.com>
2025-02-07 13:50:39 -0300

    att

--- src/url.js
+++ src/url.js
@@ -4,8 +4,15 @@ export const url = isProd
   ? "https://celulas-igreja-api.onrender.com"
   : "http://localhost:3001";
 
-/* ip da api - deprecated
+
+
+
+
+
+/* ip da api 
+
 35.160.120.126
 44.233.151.27
 34.211.200.85
+
 */
