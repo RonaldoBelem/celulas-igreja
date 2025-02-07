a13423773a087e1e56d31d5718552612a019d03a
Igreja Ranking App <igreja-ranking@example.com>
2025-02-07 13:12:36 -0300

    att

--- src/url.js
+++ src/url.js
@@ -1,6 +1,11 @@
+export const url = process.env.NODE_ENV === 'production' 
+  ? "https://celulas-igreja-api.onrender.com"  // URL do seu backend no Render
+  : "http://localhost:3001";
 
+/* ip da api 
 
+35.160.120.126
+44.233.151.27
+34.211.200.85
 
-
-
-export const url = "http://localhost:3001";
+*/
