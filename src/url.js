78044117c17dde88ba67ee0461fa80aba3a21a10
RONALDO NUNES BELEM <98597641+RonaldoBelem@users.noreply.github.com>
2025-02-06 21:47:14 -0300

    att

--- src/url.js
+++ src/url.js
@@ -3,4 +3,10 @@
 
 
 
-export const url = "https://celulas-igreja-api.onrender.com";
+
+
+
+export const url =
+  process.env.NODE_ENV === "production"
+    ? "https://celulas-igreja-api.onrender.com"
+    : "https://celulas-igreja-api.onrender.com/api/";
